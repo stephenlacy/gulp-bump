195501302bfcc0ba1ea9e0087c3d6c456a5381b7
Nasser Rafie <contact@geminorum.ir>
2017-07-27 06:27:02 +0430

    examples for WordPress plugin/theme adn php constant
    #68

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -24,6 +24,30 @@ gulp.task('xml', function(){
   .pipe(gulp.dest('./build'));
 });
 
+// WordPress Theme: https://developer.wordpress.org/themes/basics/main-stylesheet-style-css/
+gulp.task('wptheme', function(){
+  gulp.src('./style.css')
+  .pipe(bump())
+  .pipe(gulp.dest('./build'));
+});
+
+// WordPress Plugin: https://developer.wordpress.org/plugins/the-basics/header-requirements/
+gulp.task('wpplugin', function(){
+  gulp.src('./plugin.php')
+  .pipe(bump())
+  .pipe(gulp.dest('./build'));
+});
+
+// PHP Constant: `define( 'MY_PLUGIN_VERSION', '1.0.0' );`
+gulp.task('phpconstant', function(){
+  var constant = "MY_PLUGIN_VERSION";
+  gulp.src('./plugin.php')
+  .pipe(bump({
+      regex: new RegExp( "([<|\'|\"]?"+constant+"[>|\'|\"]?[ ]*[:=,]?[ ]*[\'|\"]?[a-z]?)(\\d+\\.\\d+\\.\\d+)(-[0-9A-Za-z\.-]+)?([\'|\"|<]?)", "i" ),
+    }))
+  .pipe(gulp.dest('./build'));
+});
+
 gulp.task('key', function(){
   gulp.src('./key.json')
   .pipe(bump({key: 'appversion'}))
